73f126b939068dc1dd6a63197acbe89dc2d30f4a
iam-dha <iamdha0706@gmail.com>
2025-05-06 11:07:23 +0700

    change folder name to flashcardapp

---
+++ FlashcardApp/controllers/client/flashcard.controller.js
@@ -0,0 +1,5 @@
+// Model
+const User = require("../../models/user.model");
+const Role = require("../../models/role.model");
+const Flashcard = require("../../models/flashcard.model");
+const UserInformation = require("../../models/userInformation.model");
--- FlashcardApp/routes/client/index.routes.js
+++ FlashcardApp/routes/client/index.routes.js
@@ -1,8 +1,10 @@
 const systemConfig = require("../../config/system");
 const loginRoute = require("./login.route");
 const authRoute = require("./auth.routes");
+const flashcardRoute = require("./flashcard.routes");
 module.exports = (app) => {
     app.use("/login", loginRoute);
 
     app.use(`${systemConfig.apiPath}/v1/auth`, authRoute);
+    app.use(`${systemConfig.apiPath}/v1/flashcards`, flashcardRoute);
 }
