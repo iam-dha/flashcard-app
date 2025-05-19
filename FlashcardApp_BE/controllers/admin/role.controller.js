c03469b185275cf2f0a97a79d88c6df0799641e5
iam-dha <iamdha0706@gmail.com>
2025-05-19 21:24:53 +0700

    add delete user

---
+++ FlashcardApp_BE/controllers/admin/role.controller.js
@@ -0,0 +1,39 @@
+//Models
+const Role = require("../../models/role.model");
+const Permission = require("../../models/permission.model");
+//[GET] /api/v1/admin/roles
+module.exports.getAllRoles = async (req, res) => {
+    try {
+        const roles = await Role.find({ deleted: false });
+        if (!roles || roles.length === 0) {
+            return res.status(404).json({ message: "No roles found" });
+        }
+        return res.status(200).json({
+            result: roles.length,
+            data: roles,
+        })
+    } catch (error) {
+        console.error('[GET /api/v1/admin/roles] Error:', error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+// [GET] /api/v1/admin/roles/permissions
+module.exports.getAllPermissions = async (req, res) => {
+    try {
+        const permissions = await Permission.find({ deleted: false }).select("-__v -_id");
+        if (!permissions || permissions.length === 0) {
+            return res.status(404).json({ message: "No permissions found" });
+        }
+        return res.status(200).json({
+            result: permissions.length,
+            data: permissions,
+        });
+    } catch (error) {
+        console.error('[GET /api/v1/admin/roles/permissions] Error:', error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+module.exports.test = async (req, res) => {
+    
+}
---
+++ FlashcardApp_BE/routes/admin/role.routes.js
@@ -0,0 +1,12 @@
+const experss = require("express");
+
+//Controller
+const controller = require("../../controllers/admin/role.controller");
+const router = experss.Router();
+// Middleware
+const authMiddleware = require("../../middlewares/authenticate.middleware");
+
+router.get("/",authMiddleware.checkAccessToken("Admin"), authMiddleware.checkPermission(['ACCESS_CONTROL_ADMIN']),controller.getAllRoles);
+router.get("/permissions", authMiddleware.checkAccessToken("Admin"), authMiddleware.checkPermission(['ACCESS_CONTROL_ADMIN']),controller.getAllPermissions);
+router.get("/test", controller.test);
+module.exports = router;
