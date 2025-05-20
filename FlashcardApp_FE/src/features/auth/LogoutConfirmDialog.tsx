6802e536eb1623ee156e4cd5dfb44539c365166c
mrbighead <lenhathoang2004@gmail.com>
2025-05-20 15:27:48 +0700

    feat: implement registration flow with OTP verification and password reset functionality

--- FlashcardApp_FE/src/features/auth/LogoutConfirmDialog.tsx
+++ FlashcardApp_FE/src/features/auth/LogoutConfirmDialog.tsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { AlertCircle, CircleCheck } from "lucide-react";
 import { authService } from "@/services/authService";
+import {
+  AlertDialog,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import AlertMessage from "@/components/custom-ui/AlertMessage";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
 
-export default function DeleteFolderCard({ onCancel }: { onCancel: () => void }) {
+export default function LogoutConfirmDialog() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [success, setSuccess] = useState<string | null>(null);
 
-  const handleDelete = async () => {
+  const handleLogout = async () => {
     setLoading(true);
     setError(null);
     try {
@@ -25,31 +37,25 @@ export default function DeleteFolderCard({ onCancel }: { onCancel: () => void })
   };
 
   return (
-    <div className="bg-card w-lg rounded-lg p-6 shadow-md">
-      <p className="mb-2 text-xl font-bold">Log out</p>
-      <p className="">
-        Are you sure you want log out?
-      </p>
-      {error && (
-        <div className="bg-destructive/15 text-destructive mt-6 flex items-center gap-2 rounded-md p-3 text-sm">
-          <AlertCircle className="h-4 w-4" />
-          {error}
-        </div>
-      )}
-      {success && (
-        <div className="mt-4 mt-6 flex items-center gap-2 rounded-md bg-green-200 p-3 text-sm text-green-500 dark:bg-green-700 dark:text-green-300">
-          <CircleCheck className="h-4 w-4" />
-          {success}
-        </div>
-      )}
-      <div className="mt-6 flex items-center justify-end gap-4">
-        <Button variant="outline" onClick={onCancel} disabled={loading}>
-          Cancel
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button className="hover:bg-destructive/15 text-destructive justify-start rounded-lg bg-transparent">
+          <LogOut />
+          Log out
         </Button>
-        <Button className="hover:bg-black/70 dark:hover:bg-white/50" onClick={handleDelete} disabled={loading}>
-          {loading ? "Logging out..." : "Log out"}
-        </Button>
-      </div>
-    </div>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Log out</AlertDialogTitle>
+          <AlertDialogDescription>Are you sure you want to log out?</AlertDialogDescription>
+          {success && <AlertMessage type="success" message={success} />}
+          {error && <AlertMessage type="error" message={error} />}
+        </AlertDialogHeader>
+        <AlertDialogFooter className="space-x-2">
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={handleLogout}>{loading ? "Logging out..." : "Log out"}</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
--- FlashcardApp_FE/src/services/authService.ts
+++ FlashcardApp_FE/src/services/authService.ts
@@ -1,12 +1,13 @@
 import api from "@/services/api";
 import { UserTypes, RegisterRequestTypes, LoginRequestTypes, AuthResponseTypes } from "@/types/user.types";
+import Cookies from "js-cookie";
 
 class AuthService {
   private accessToken: string = "accessToken";
 
   async register(userData: RegisterRequestTypes): Promise<AuthResponseTypes> {
     try {
-      const response = await api.post("/auth/register", userData);
+      const response = await api.post("/auth/register/verify", userData);
       this.handleAuthentication(response.data);
       return response.data;
     } catch (error: any) {
@@ -23,6 +24,38 @@ class AuthService {
     } catch (error: any) {
       throw new Error("From API: Failed to login");
     }
+  }
+
+  async requestOtp(email: string): Promise<void> {
+    try {
+      await api.post("/auth/register/request-otp", { email });
+    } catch (error: any) {
+      console.error("Request OTP error:", error);
+      throw new Error(error.response?.data?.message || "Failed to request OTP");
+    }
+  }
+
+  async verifyOtp(email: string, otp: string): Promise<{token: string}> {
+    try {
+      const response = await api.post("/auth/register/verOtp", { email, otp });
+      return { token: response.data.token };
+    } catch (error: any) {
+      console.error("Verify OTP error:", error);
+      throw new Error(error.response?.data?.message || "Failed to verify OTP");
+    }
+  }
+
+  async forgotPassword(email: string): Promise<void> {
+    try {
+      await api.post("/auth/forgot-password", { email });
+    } catch (error: any) {
+      console.error("Forgot password error:", error);
+      throw new Error(error.response?.data?.message || "Failed to send password reset email");
+    }
+  }
+
+  async resetPassword(token: string, newPassword: string, reNewPassword: string) {
+    return api.post(`/auth/reset-password/${token}`, { newPassword, reNewPassword });
   }
 
   private handleAuthentication(authData: AuthResponseTypes): void {
@@ -42,9 +75,11 @@ class AuthService {
     }
   }
 
-  logout(): void {
+  async logout(): Promise<void> {
+    // await api.post("/auth/logout");
     localStorage.removeItem(this.accessToken);
     localStorage.removeItem("role");
+    Cookies.remove("refreshToken");
   }
 
   getAccessToken(): string | null {
